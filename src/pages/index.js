f2c90c0983dde84f2bf97edb0ef17cface642702
alimnh520 <alimnh412@gmail.com>
2026-01-25 21:38:16 +0600

    add all

--- src/pages/index.js
+++ src/pages/index.js
@@ -146,7 +146,7 @@ export default function Chat() {
 
     const isMe = msg.senderId === user._id;
     const otherUserId = isMe ? msg.receiverId : msg.senderId;
-    const otherUser = allUser.find(u => u._id === msg.receiverId);
+    const otherUser = allUser.find(u => u._id === msg.senderId);
     setHistory(prev => {
       const prevList = Array.isArray(prev) ? [...prev] : [];
       const index = prevList.findIndex(h =>
