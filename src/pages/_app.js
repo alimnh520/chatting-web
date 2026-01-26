901b7025e769ef56d444f00731d1095767174c10
alimnh520 <alimnh412@gmail.com>
2026-01-27 00:35:58 +0600

    pwa update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,28 +1,13 @@
 import "@/styles/globals.css";
 import Head from "next/head";
 import Provider from "./Provider";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  const [deferredPrompt, setDeferredPrompt] = useState(null);
-
   useEffect(() => {
     if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/sw.js")
-        .then(() => console.log("✅ Service Worker registered"))
-        .catch((err) => console.log("❌ SW registration failed:", err));
+      navigator.serviceWorker.register("/sw.js");
     }
-
-    const handleBeforeInstallPrompt = (e) => {
-      e.preventDefault();
-      setDeferredPrompt(e);
-    };
-
-    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
-
-    return () =>
-      window.removeEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
   }, []);
 
   return (
@@ -30,16 +15,17 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>My Chat</title>
         <meta name="description" content="Realtime Chat PWA" />
+
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, height=device-height, viewport-fit=cover"
         />
+
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="theme-color" content="#000000" />
       </Head>
 
       <Provider>
-        <Component {...pageProps} deferredPrompt={deferredPrompt} />
+        <Component {...pageProps} />
       </Provider>
     </>
   );
